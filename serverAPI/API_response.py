15a6e8b706119cede844be8b13f3bfb909a48a4a
IvanDev <123570888+IvanDev-ai@users.noreply.github.com>
2024-03-01 14:11:59 +0100

    Update API_response.py

--- serverAPI/API_response.py
+++ serverAPI/API_response.py
@@ -1,39 +1,10 @@
-#Instalaciones necesarias para el google colab 
-
-!pip install transformers
-!pip install transformers --upgrade
-!pip install mtranslate
-!pip install torch
-!pip install langdetect
-!pip install flask
-!pip install flask_cors
- 
-!pip install pyngrok
-
- 
-!ngrok authtoken 2QqsUKuyaix37IWHno2HikaVqrf_5C8Up2BuMbsdhAzZAEhjP
-# Importar las bibliotecas necesarias
-from pyngrok import ngrok
-from transformers import GPT2Tokenizer, GPT2LMHeadModel
-from mtranslate import translate
+import streamlit as st
 import torch
+from mtranslate import translate
+from transformers import GPT2LMHeadModel, GPT2Tokenizer
 from langdetect import detect
-from flask import Flask, request, jsonify
-from flask_cors import CORS
-import os
- 
-# Crear una instancia de la aplicación Flask para crear un servidor
-app = Flask(__name__)
- 
-# Habilitar el CORS (Compartir Recursos de Origen Cruzado) para permitir solicitudes desde cualquier origen
-CORS(app, resources={r"/*": {"origins": "*"}})
- 
-#Definir la ruta del endpoint "/generate-response/<question>" para generar la respuesta recibiendo en la url la pregunta como "question"
-@app.route('/generate-response/<question>', methods=['POST'])
+import gdown
 def generate_response(question):
-  # Obtener la pregunta del cuerpo de la solicitud en formato JSON
-    question = request.json['question']
- 
     # Verificar si hay una GPU disponible para utilizarla, de lo contrario utilizar la CPU
     device = "cuda" if torch.cuda.is_available() else "cpu"
  
@@ -54,8 +25,13 @@ def generate_response(question):
     # Carga el modelo de GPT2
     model = GPT2LMHeadModel.from_pretrained("gpt2")
     model.resize_token_embeddings(len(tokenizer))
+
     #Carga el estado del modelo preentrenado con las bases de datos de piscologia al modelo de gpt2
-    model.load_state_dict(torch.load("/content/drive/MyDrive/TFG-FINAL/pyschoGPT.pt", map_location=device))
+    url = 'https://drive.google.com/uc?id=1YKCC8flDS3rL8g1aduOpp00IwWO223hR'
+    output_model = 'pyschoGPT.pt'
+    gdown.download(url, output_model, quiet=False)
+
+    model.load_state_dict(torch.load(output_model, map_location=device))
     model.to(device)
     model.eval()
  
@@ -79,17 +55,16 @@ def generate_response(question):
     if start_index != -1 and end_index != -1:
         output_text = output_text[start_index + len(start_token):end_index].strip()
  
-    # Devolver la respuesta generada como JSON
-    return jsonify({'response': output_text})
- 
-if __name__ == '__main__':
-    # Obtener el puerto aleatorio disponible en el entorno o utilizar el puerto 5000 por defecto
-    port = int(os.environ.get('PORT', 5000))
- 
-    # Iniciar el túnel de Ngrok para crear una URL pública y accesible desde Internet
-    # Este paso es necesario ya que desde google colab no se puede hacer el server flask ya que lo bloquea entonces usamos un intermediario.
-    ngrok_tunnel = ngrok.connect(port)
-    print('Public URL:', ngrok_tunnel.public_url)
- 
-    # Ejecutar la aplicación Flask utilizando la URL proporcionada por Ngrok
-    app.run(host='0.0.0.0', port=port)
+    # Devolver la respuesta generada
+    return output_text
+
+# Streamlit UI
+st.title("Pregunta a PsychoGPT")
+
+# Recoger la pregunta del usuario
+question = st.text_input("Haz una pregunta")
+
+if st.button("Generar respuesta"):
+    # Generar la respuesta
+    response = generate_response(question)
+    st.write(" ", response)
